278cae3a5ddbf9f17daad08327bb2021344eb11f
fernandoselvino@gmail.com <fernandoselvino@gmail.com>
2020-01-23 22:03:31 -0300

    Update do usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -16,9 +16,26 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
 
-    return res.json({ ok: true });
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+      if (userExists) {
+        return res.status(400).json({ erro: 'User already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(400).json({ erro: 'Password does not match' });
+    }
+    const { id, name, provider } = await user.update(req.body);
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 export default new UserController();
