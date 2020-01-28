b5731734ff9fa5db95c1dbcd190626e94048800d
fernandoselvino@gmail.com <fernandoselvino@gmail.com>
2020-01-28 14:46:55 -0300

    continuando API - Agendamento de serviço

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -28,19 +29,47 @@ class AppointmentController {
         .json({ error: 'You can only create appointments with providers' });
     }
     /**
+     * parseISO transforma a string date em um objeto javascript e
+     * esse objeto sera utilizado dentro de strartOfHour
+     * startOfHour vai pegar somento a parte da hora despresando o restante
+     * minutos e segundos
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    /**
+     * verificar se hourStarte esta antes da hora atual
+     * se retornar verdadeiro quer dizer que a data em que o usuario quer
+     * cadastrar ja passou, dai retornamos erro
+     */
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    /**
+     * Verificar se o provider tem seu horario disponivel ou se ja esta agendado
+     * se a consulta retornar valor entra no if e retorna o erro
+     */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+    /**
      * persiste os dados na tabela appointment
      */
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
     return res.json(appointment);
   }
 }
 export default new AppointmentController();
-/**
- *
- *
- *
- */
